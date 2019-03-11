9a247f4c755d57de1714688dce7a16b69b37d2dd
ShaileshSSC <shaileshchotoe2001@gmail.com>
2019-03-11 14:06:17 +0100

    Soup kan van kleur veranderen

---
+++ Mythe-Nisse/Assets/ShaiScripts/ChangeSoupColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSoupColor : ColorManager
+{
+
+    private void Start()
+    {
+
+        SetColorRGBA(this.gameObject,1f,1f,1f,1f);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag.Contains("Ingredient"))
+        {
+
+            MixColor(other.gameObject,this.gameObject);
+
+        }
+
+    }
+}
---
+++ Mythe-Nisse/Assets/ShaiScripts/NotitieColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotitieColor : ColorManager
+{
+    void Start()
+    {
+
+        SetColorRGBA(this.gameObject, Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1);
+
+    }
+
+}
