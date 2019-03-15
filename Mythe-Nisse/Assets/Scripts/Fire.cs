39378bf8db2c99a0b31c6d6c1b30549f906d59d1
jochemlam <30724588+jochemlam@users.noreply.github.com>
2019-03-15 11:52:32 +0100

    created a placeholder for the match and created starting point for the script (previous version needed rework)

---
+++ Mythe-Nisse/Assets/Scripts/Fire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private bool isHolding;
+    public bool IsHolding { get; set; }
+
+    private float timer;
+    private readonly float maxTimer = 1;
+
+    private Rigidbody rb;
+    private ParticleSystem fireParticles;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        fireParticles = GetComponent<ParticleSystem>();
+    }
+
+    private void Ignite()
+    {
+        var em = fireParticles.emission;
+        em.enabled = true;
+    }
+
+    private void Extinguish()
+    {
+        var em = fireParticles.emission;
+        em.enabled = false;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            if (isHolding)
+            {
+
+            }
+            Extinguish();
+        }
+        
+    }
+
+}
