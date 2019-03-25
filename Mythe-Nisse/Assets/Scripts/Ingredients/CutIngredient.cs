71f370eff741a6f0d2bc3a0b611d3d4fd4b7139e
AlexJanson <alex@hoofddorp.nl>
2019-03-25 11:19:54 +0100

    Snijden completed

---
+++ Mythe-Nisse/Assets/Scripts/Ingredients/CutIngredient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutIngredient : MonoBehaviour
+{
+    public GameObject ingredientSmall;
+    [Range(1,10)]
+    public int amountToSpawn = 3;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Knife") {
+            for (int i = 0; i < amountToSpawn; i++) {
+                Destroy(this.gameObject);
+                GameObject temp = Instantiate(ingredientSmall, transform.position, Quaternion.identity);
+                Color parentColor = GetComponent<Ingredient>().objectColor;
+                var parentType = GetComponent<Ingredient>().objectType;
+                temp.GetComponent<Renderer>().material.color = parentColor;
+                temp.GetComponent<Ingredient>().objectColor = parentColor;
+                temp.GetComponent<Ingredient>().objectType = parentType;
+                temp.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            }
+        }
+    }
+}
