0f9c35ba86e25706355349b1f1e8e44e8934aa4d
MLGC00KIE <24893@ma-web.nl>
2019-03-26 14:11:55 +0100

    fixed artist scene

---
+++ Mythe-Nisse/Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    float TimeLeft = 180f; // 3 minutes default
+
+    public static Action onTimeChanged;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLeft -= Time.deltaTime;
+        if(onTimeChanged != null)
+        {
+            onTimeChanged();
+        }
+    }
+}
