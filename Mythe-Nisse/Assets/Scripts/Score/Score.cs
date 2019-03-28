7ec34b466e30ebb207b6b7c9cc8d51cb950d8f15
Mitchell Tuinstra <24893@ma-web.nl>
2019-03-29 00:07:07 +0100

    added scoresystem based on time and color accuracy

---
+++ Mythe-Nisse/Assets/Scripts/Score/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private Timer timer;
+
+    private void Start()
+    {
+        timer = GetComponentInChildren<Timer>();
+    }
+
+    public int getFinalScore()
+    {
+        int finalScore = 0;
+        finalScore += (int)timer.GetTimeLeft();
+
+        //calculate how accurate the soup color is
+        // int soupAccuracy = ????
+        // finalScore += soupAccuracy
+
+        return finalScore;
+    }
+}
