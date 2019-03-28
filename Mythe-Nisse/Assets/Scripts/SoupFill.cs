8ff965a5b6239fb02556abfbd78fcf67a91e60b2
AlexJanson <alex@hoofddorp.nl>
2019-03-28 09:29:15 +0100

    Water percentage shader

---
+++ Mythe-Nisse/Assets/Scripts/SoupFill.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoupFill : MonoBehaviour
+{
+    Renderer rend;
+    [Range(0f, 1f)]
+    public float fillAmount = 0.50f;
+    public float fillAmountPercent;
+    public Color stdColor;
+    
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    
+    void Update()
+    {
+        fillAmountPercent = fillAmount * 0.551f;
+        fillAmountPercent = Mathf.Lerp(0.45f, 0.551f, fillAmount);
+        rend.material.SetFloat("_FillAmount", fillAmountPercent);
+    }
+
+    public void Empty()
+    {
+        fillAmount = 1f;
+    }
+
+    public void Fill()
+    {
+        fillAmount = 0f;
+    }
+}
