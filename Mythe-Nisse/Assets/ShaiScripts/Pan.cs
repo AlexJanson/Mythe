22e86703282c685b04086c38a9c4b3c316665d4e
ShaileshSSC <shaileshchotoe2001@gmail.com>
2019-03-29 09:53:37 +0100

    pan script en win condition

---
+++ Mythe-Nisse/Assets/ShaiScripts/Pan.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Pan : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.tag.Contains("Tafel"))
+        {
+
+            ColorManager.CheckColorMatch(GameObject.Find("Soup"), GameObject.Find("Blaadje"));
+
+        }
+
+    }
+
+}
