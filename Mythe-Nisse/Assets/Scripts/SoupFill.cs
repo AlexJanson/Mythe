9d291b99ec552bc9fd096a0387ffd12602b878a4
AlexJanson <alex@hoofddorp.nl>
2019-04-01 10:07:54 +0200

    Changed soupfill script to reset color once empty

--- Mythe-Nisse/Assets/Scripts/SoupFill.cs
+++ Mythe-Nisse/Assets/Scripts/SoupFill.cs
@@ -22,6 +22,7 @@ public class SoupFill : MonoBehaviour
         fillAmountPercent = Mathf.Lerp(0.451f, 0.551f, fillAmount);
         if(fillAmount >= 0.99f) {
             rend.material.SetFloat("_FillAmount", 1f);
+            rend.material.SetColor("_Tint", new Color(0, 0, 0, 0));
         } else
             rend.material.SetFloat("_FillAmount", fillAmountPercent);
     }
