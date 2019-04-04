1350456c28bce81e26087f442fdfd4883074c4e0
AlexJanson <alex@hoofddorp.nl>
2019-04-04 09:07:34 +0200

    Hand texture has been implemented

---
+++ Mythe-Nisse/Assets/DisableTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTutorial : MonoBehaviour
+{ 
+    public float delay;
+
+    private void Start()
+    {
+        StartCoroutine(DisableCanvas());
+    }
+
+    IEnumerator DisableCanvas()
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Disabling the canvas!");
+        gameObject.SetActive(false);
+        yield return null;
+    }
+}
