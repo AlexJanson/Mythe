d01f9f20e940246db6c8a35c195ed367a8ecbc75
jochemlam <30724588+jochemlam@users.noreply.github.com>
2019-04-09 12:02:57 +0200

    need testing with vive, cant test now cuz of null reference errors which are caused by not having a vive connected

--- Mythe-Nisse/Assets/Scripts/VR_UI_Button.cs
+++ Mythe-Nisse/Assets/Scripts/VR_UI_Button.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(RectTransform))]
 public class VR_UI_Button : MonoBehaviour
 {
     private BoxCollider boxCollider;
     private RectTransform rectTransform;
+
+    private bool canActivate = false;
+    private bool isInStart = false;
+    private bool isInQuit = false;
 
     private void OnEnable()
     {
@@ -27,5 +33,59 @@ public class VR_UI_Button : MonoBehaviour
         }
 
         boxCollider.size = rectTransform.sizeDelta;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CheckButton();
+        canActivate = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CheckButton();
+        canActivate = false;
+    }
+
+    private void CheckButton()
+    {
+        switch (gameObject.name)
+        {
+            case "Start":
+                isInStart = !isInStart;
+                break;
+            case "Quit":
+                isInQuit = !isInQuit;
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (canActivate)
+        {
+            if (SteamVR_Actions._default.InteractUI.GetStateDown(SteamVR_Input_Sources.Any) || Input.GetMouseButtonDown(0))
+            {
+                if (isInStart)
+                {
+                    StartGame("Main");
+                }
+
+                if (isInQuit)
+                {
+                    QuitGame();
+                }
+            }
+        }
+    }
+
+    public void StartGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
